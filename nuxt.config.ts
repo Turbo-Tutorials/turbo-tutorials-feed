8762d2e85ed305648c5bc3a7b0fccf205c5d0c97
Tim Benniks <tbenniks@gmail.com>
2023-08-14 16:23:58 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      contentfulSpaceId: process.env.CONTENTFUL_SPACE_ID,
+      contentfulDeliveryApiKey: process.env.CONTENTFUL_DELIVERY_API_KEY,
+      contentfulEnvironment: process.env.CONTENTFUL_ENVIRONMENT,
+      hostname: "https://turbo-tutorials-feed.vercel.app"
+    },
+  },
+})
---
+++ server/routes/feed.xml.ts
@@ -0,0 +1,63 @@
+import RSS from 'rss';
+import { getContentfulClient, enhanceContentfulItem } from "../../helpers"
+
+export default defineEventHandler(async (event) => {
+
+  const {
+    public: { hostname },
+  } = useRuntimeConfig();
+
+  const feed = new RSS({
+    title: 'Turbo Tutorials',
+    generator: 'Turbo Tutorials',
+    description: 'You can find short instructional videos at Turbo Tutorials that teach you the most common things in modern JavaScript and its meta frameworks.',
+    site_url: `${hostname}/`,
+    feed_url: `${hostname}/feed.xml`,
+    image_url: 'https://res.cloudinary.com/dwfcofnrd/image/upload/w_1200,q_auto,f_auto/Turbo%20Tutorials/share-image.png',
+    language: 'en',
+    custom_namespaces: {
+      'g': 'http://base.google.com/ns/1.0'
+    },
+  });
+
+  const ctfClient = getContentfulClient();
+  const tutorialData = await ctfClient.getEntries({
+    content_type: "turboTutorial",
+  });
+
+  const tutorials = tutorialData.items.map((tut: any) => {
+    const tutorial = enhanceContentfulItem(tut)
+    return {
+      url: `/tutorials/${tutorial.slug}`,
+      title: tutorial.title,
+      date: tutorial.publicationDate,
+      description: tutorial.description,
+      categories: tutorial.tags,
+      complexity: tutorial.complexity,
+      id: tutorial.videoId,
+      image: tutorial.poster.src
+    }
+  })
+
+  tutorials.forEach((tutorial: any) => {
+    feed.item({
+      title: tutorial.title ?? '-',
+      url: `${hostname}${tutorial.url}/`,
+      date: tutorial.publicationDate,
+      description: tutorial.description,
+      categories: tutorial.categories,
+      author: 'Tim Benniks',
+      custom_elements: [
+        { 'g:id': tutorial.id },
+        { 'g:image_link': tutorial.image },
+        { 'g:price': 0 },
+        { complexity: tutorial.complexity },
+        { link: `${hostname}${tutorial.url}/` },
+      ]
+    });
+  });
+
+  const feedString = feed.xml({ indent: true });
+  event.node.res.setHeader('content-type', 'text/xml');
+  event.node.res.end(feedString);
+});
